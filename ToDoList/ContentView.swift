bcd37e3c81c942abdfc5e8c2306eef3bc94c4719
PoojaK <poojakarthiks28@gmail.com>
2025-07-28 12:51:26 -0500

    connected eveything and added importance

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -34,15 +34,21 @@ struct ContentView: View {
             Spacer()
             List{
                 ForEach(toDos){ toDoItem in
-                    Text(toDoItem.title)
+                    if toDoItem.isImportant{
+                        Text("‼️" + toDoItem.title)
+                    } else {
+                        Text(toDoItem.title)
+                    }
                 }
             }
+            .listStyle(.plain)
         }
         if showNewTask{
-            NewToDoView()
+            NewToDoView(showNewTask: $showNewTask, toDoItem: ToDoItem(title:"", isImportant: false))
         }
     }
 }
 #Preview {
     ContentView()
+        .modelContainer(for: ToDoItem.self, inMemory: true)
 }
--- ToDoList/NewToDoView.swift
+++ ToDoList/NewToDoView.swift
@@ -6,32 +6,41 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct NewToDoView: View {
+    @Binding var showNewTask: Bool
+    @Bindable var toDoItem: ToDoItem
+    @Environment(\.modelContext) var modelContext
     var body: some View {
         VStack{
             Text("Task title:")
                 .font(.title)
                 .fontWeight(.bold)
-            TextField("Enter the task description...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+            TextField("Enter the task description...", text: $toDoItem.title, axis: .vertical)
                 .padding()
                 .background(Color(.purple))
                 .cornerRadius(15)
                 .padding()
-            Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+            Toggle(isOn: $toDoItem.isImportant) {
                 Text("Is it important?")
             }
             .padding()
             Button {
-
+                addToDo()
+                showNewTask = false
             } label: {
                     Text("Save")
             }
 
         }
     }
+    func addToDo(){
+        let toDo = ToDoItem(title: toDoItem.title, isImportant: toDoItem.isImportant)
+        modelContext.insert(toDo)
+    }
 }
 
 #Preview {
-    NewToDoView()
+    NewToDoView(toDoItem: ToDoItem(title: "", isImportant: false))
 }
